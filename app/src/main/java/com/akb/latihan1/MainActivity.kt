7bfb46195f6f11ba6b87db8557ff264e2addc8f1
Rizka Ghinna Auliya <rizkaghina29@gmail.com>
2023-04-11 04:18:25 +0700

    akb assignment

---
+++ app/src/main/java/com/akb/latihan1/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.akb.latihan1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Tanggal Pengerjaan : 10/04/2023
+ * NIM : 10120918
+ * Nama : Dennie Fuzi Alviyannies
+ * Kelas : IF-10
+ * */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
